b7d5de9500fa4f6cfa5eb9bf771692ec7ff80903
kshvab <kshvab@gmail.com>
2019-01-01 23:28:51 +0200

    1.0.5 .env set up (hacker protection)

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
+const dotenv = require('dotenv');
+const path = require('path');
+
+const root = path.join.bind(this, __dirname);
+dotenv.config({ path: root('.env') });
+
 module.exports = {
-  PORT: process.env.PORT || 3000, //можна змінити при запуску використовуючи змінну оточення
-  MONGO_URL:
-    "mongodb+srv://cdcminingmonitor:V8WwTZtiV7la2fN8@cdccluster-l4r4u.mongodb.net/ShopDbCluster?retryWrites=true"
+  PORT: process.env.PORT || 3000,
+  MONGO_URL: process.env.MONGO_URL,
+  IS_PRODUCTION: process.env.NODE_ENV === 'production'
 };
